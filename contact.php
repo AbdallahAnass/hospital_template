964aea1942807daed85998d7d4e78bc68a0cfc97
cisco1cisco <anasabdalla99@gmail.com>
2024-07-30 21:52:17 +0300

    Added the php files

---
+++ contact.php
@@ -0,0 +1,50 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Engineer";
+$db_name = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create TABLE
+$sql = "CREATE TABLE IF NOT EXISTS CONTACT (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    subject TEXT,
+    message TEXT
+);";
+
+$conn->query($sql);
+
+// Getting user data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$Subject = $_POST['Subject'];
+$Message = $_POST['Message'];
+
+// INSERTING INTO TABLE
+$stmt = $conn->prepare("INSERT INTO CONTACT 
+(name, email, subject, message) 
+VALUES (?, ?, ?, ?)");
+
+$stmt->bind_param("ssss", $name, $email, $Subject, $Message);
+
+$stmt->execute();
+
+
+// Close the connection
+$stmt->close();
+$conn->close();
+
+// Printing SUCCESS MESSAGE
+header("Location: http://localhost/hospital/");
+
+?>
---
+++ submit.php
@@ -0,0 +1,54 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Engineer";
+$db_name = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create TABLE
+$sql = "CREATE TABLE IF NOT EXISTS Patients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    medical_condition TEXT,
+    department VARCHAR(100),
+    doctor VARCHAR(100),
+    email VARCHAR(100),
+    comment TEXT
+);";
+
+$conn->query($sql);
+
+// Getting user data
+$name = $_POST['name'];
+$condition = $_POST['condition'];
+$department = $_POST['department'];
+$doctor = $_POST['doctor'];
+$email = $_POST['email'];
+$Comment = $_POST['Comment'];
+
+// INSERTING INTO TABLE
+$stmt = $conn->prepare("INSERT INTO Patients 
+(name, medical_condition, department, doctor, email, comment) 
+VALUES (?, ?, ?, ?, ?, ?)");
+
+$stmt->bind_param("ssssss", $name, $condition, $department, $doctor, $email, $comment);
+
+$stmt->execute();
+
+
+// Close the connection
+$stmt->close();
+$conn->close();
+
+// Printing SUCCESS MESSAGE
+header("Location: http://localhost/hospital/");
+
+?>
